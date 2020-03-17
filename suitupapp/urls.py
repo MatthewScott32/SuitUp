38daeee0e37c3f423b2dc9fa765a5e1746a944e9
Matthew Blagg <matthewscottblagg@gmail.com>
2020-03-16 21:46:05 -0500

    created login

--- suitupapp/urls.py
+++ suitupapp/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from .views import purchased_item_list
 from .views import stores_list
 from .views.home.home import home
-
+from .views.auth.logout import logout
 app_name = "suitupapp"
 
 urlpatterns = [
     path('home/', home, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', logout, name='logout'),
     path('purchaseditems/', purchased_item_list, name='purchaseditems'),
     path('stores/', stores_list, name='stores'),
 ]
